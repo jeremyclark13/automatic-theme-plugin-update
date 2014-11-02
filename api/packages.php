897b621ee859158e722c68d2fbd500a8f45fb3f8
Dzoni <johnnynt3@gmail.com>
2014-11-02 20:40:10 +0100

    Plugin homepage
    Array key "homepage" is updated to "plugin_homepage" in order to achieve this: http://prntscr.com/52ds65

--- api/packages.php
+++ api/packages.php
@@ -31,7 +31,7 @@ $packages['test-plugin-update'] = array( //Replace plugin with the plugin slug t
             'author' => 'Author Name', //Author name - can be linked using html - <a href="http://link-to-site.com">Author Name</a>
             'requires' => '2.8', // WP version required for plugin
             'tested' => '3.0.1', // WP version tested with
-            'homepage' => 'http://your_plugin_website', // Site devoted to your plugin if available
+            'plugin_homepage' => 'http://your_plugin_website', // Site devoted to your plugin if available
             'downloaded' => '1000', // Number of times downloaded
             'external' => '', // Unused
             //plugin.zip is the same as file_name
